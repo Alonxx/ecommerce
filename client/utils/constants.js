cebf978e242e2e9fe3b4f1fb53974666873e1c55
Alonso Diaz <alonsodiazlip@gmail.com>
2021-05-14 15:05:17 -0300

    Add files via upload

---
+++ client/utils/constants.js
@@ -0,0 +1,33 @@
+export const ActionTypes = {
+	GET_PRODUCTS: 'GET_PRODUCTS',
+	PRODUCT_DETAIL: 'PRODUCT_DETAIL',
+	PRODUCT_CREATED: 'PRODUCT_CREATED',
+	GET_PRODUCTS_QUERY: 'GET_PRODUCTS_QUERY',
+	UPDATE_PRODUCT: 'UPDATE_PRODUCT',
+	EMPTY_PRODUCT_CREATED: 'EMPTY_PRODUCT_CREATED',
+
+	GET_ORDERS: 'GET_ORDERS',
+	ORDER_DETAIL: 'ORDER_DETAIL',
+
+	GET_CATEGORIES: 'GET_CATEGORIES',
+
+	GET_BRANDS: 'GET_BRANDS',
+	UPDATE_BRAND: 'UPDATE_BRAND',
+
+	ADD_DB_PRODUCT_CART: 'ADD_DB_PRODUCT_CART',
+	ADD_PRODUCT_CART: 'ADD_PRODUCT_CART',
+	REMOVE_PRODUCT_CART: 'REMOVE_PRODUCT_CART',
+
+	CREATE_USER: 'CREATE_USER',
+	LOGIN_USER: 'LOGIN_USER',
+	CLEAN_SIGNUP: 'CLEAN_SIGNUP',
+};
+
+export const URLS = {
+	URL_PRODUCTS: 'https://storeft11g01server.herokuapp.com/products',
+	URL_USER_ORDERS: 'https://storeft11g01server.herokuapp.com/orders',
+	URL_CATEGORIES: 'https://storeft11g01server.herokuapp.com/categories',
+	URL_BRANDS: 'https://storeft11g01server.herokuapp.com/brands',
+	URL_SIGNUP: 'https://storeft11g01server.herokuapp.com/signup',
+	URL_LOGIN: 'https://storeft11g01server.herokuapp.com/login',
+};
